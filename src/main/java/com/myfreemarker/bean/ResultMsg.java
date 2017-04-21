7bc4a499e373a6cba361c64e9cd6f7b3ec9c547c
Travel <824079543@qq.com>
2017-04-21 20:47:35 +0800

    * 修改任务模板 ;

---
+++ src/main/java/com/myfreemarker/bean/ResultMsg.java
@@ -0,0 +1,15 @@
+package com.myfreemarker.bean;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/4/17.
+ */
+public class ResultMsg {
+    private String code;
+    private String desc;
+    private Object object;
+
+
+
+}
